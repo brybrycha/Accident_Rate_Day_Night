03bb53f91cfcc103e3caca968592e2f32e8eff7a
Bryan <brcha@ucsd.edu>
2024-06-03 23:00:55 -0700

    d

---
+++ src/Components/Mapdetail.svelte
@@ -0,0 +1,45 @@
+<section>
+  <p class="body-text" >
+    <br>
+    (Author considered <span class='level'>6:00AM to 6:00 PM</span> as <span class='highlight'>Daytime</span> and <span class='level'>6:00PM to 6:00 AM</span> as <span class='highlight'>Nighttime</span>)
+    With interactive map visualization, we can retrieve several facts.
+    <br><br>
+
+    <list >
+        <li><span class='highlight'>Daytime</span> has higher counts of accidents than nighttime as total accident counts in daytime is <span class='level'>4966</span>, 
+            while nighttime has total accident counts of <span class='level'>2846</span></li>
+        <br>
+        <li>Intersections have higher accident rates than any other types of road</li>
+        <br>
+    </list>
+    <br>
+    These patterns show that Daytime has a higher accident rate due to high volume of car traffic during daytime while there is significantly less people driving around at nighttime.
+    Also, intersections have the highest accident rate due to several reasons including speeding, failure to notice other cars, and confusion with traffic lights' signals.
+    <br><br>
+  </p>
+</section>
+
+<style>
+    section {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        flex-direction: column;
+        padding: 20px; /* Adjust as necessary */
+    }
+    .level {
+        color: blue;
+    }
+    .underline {
+        text-decoration: underline;
+    }
+    .highlight {
+        font-size: 1em; /* Increase the font size slightly */
+        font-weight: bold; /* Make the text bold */
+    }
+    .body-text {
+        max-width: 800px; /* Adjust as necessary */
+        margin: auto;
+    }
+</style>
