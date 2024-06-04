274964c10d8148a70230dad5b6854e5cf95249ee
Bryan <brcha@ucsd.edu>
2024-06-03 22:45:24 -0700

    d

--- src/Components/LineExp.svelte
+++ src/Components/LineExp.svelte
@@ -1,5 +1,5 @@
 <section>
-  <p class="body-text">
+  <p class="body-text" align = "center">
     <br>
     The visualization above depicts the accident rates at various times throughout the day. 
     From the chart, we can observe distinct peak times:
